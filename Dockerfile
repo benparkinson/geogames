355fd620e51fc3462942fcfb3b988959286993a2
Ben Parkinson <benjamesparkinson@gmail.com>
2023-04-07 15:49:16 +0200

    Fix river shapes

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM maven:3.8.3-openjdk-17 as build
 COPY src /usr/src/app/src
 COPY pom.xml /usr/src/app
 COPY client /usr/src/app/client
-RUN mvn -f /usr/src/app/pom.xml clean install
+RUN mvn -f /usr/src/app/pom.xml clean install -DskipTests
 
 FROM openjdk:17.0.1-jdk-slim
 COPY --from=build /usr/src/app/target/geogames-0.0.1-SNAPSHOT.jar /usr/app/geogames-0.0.1-SNAPSHOT.jar
--- src/main/java/com/parkinson/benjamin/geogames/service/RiverLoaderService.java
+++ src/main/java/com/parkinson/benjamin/geogames/service/RiverLoaderService.java
@@ -6,26 +6,32 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.context.ApplicationContext;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RiverLoaderService extends FileLoaderService {
 
-  public RiverLoaderService() {
+  private final ApplicationContext applicationContext;
+
+  @Autowired
+  public RiverLoaderService(ApplicationContext applicationContext) {
     super();
+    this.applicationContext = applicationContext;
   }
 
   @Cacheable("rivers")
   public List<GeoData> loadRivers() throws IOException {
-    File riversDir = new ClassPathResource("data/rivers").getFile();
-    File[] files = riversDir.listFiles();
+    Resource[] resources = applicationContext.getResources("classpath*:/data/rivers/*.geo.json");
 
     List<GeoData> rivers = new ArrayList<>();
 
-    for (File file : files) {
-      rivers.add(readFile(new FileInputStream(file), GeoData.class));
+    for (Resource resource : resources) {
+      rivers.add(readFile(resource.getInputStream(), GeoData.class));
     }
 
     return rivers;
