91ad761ab64fefbd100726186d3c5c4edeb57598
Ben Parkinson <benjamesparkinson@gmail.com>
2023-04-07 16:51:16 +0200

    Add more rivers

--- src/main/resources/data/rivers/processRiverImport.sh
+++ src/main/resources/data/rivers/processRiverImport.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 INPUTFILE=$1
-OUTPUTFILE=$2
+OUTPUTFILE=$2.geo.json
 
-tail -n +7 $INPUTFILE | head -n -2 | sed '/"@id":\|"alt_name:ku":\|"name:\|"id":\|"source:name\|"wikidata":\|"wikipedia":\|"alt_name":\|"alt_name:\|"destination:lang\|"gvr:code":\|"int_name":\|"is_in:country_code":\|"wikipedia:\|"code":/d' > $OUTPUTFILE
+tail -n +7 $INPUTFILE | head -n -2 | sed '/"@id":\|"alt_name:ku":\|"name:\|"id":\|"source:name\|"wikidata":\|"wikipedia":\|"alt_name":\|"alt_name:\|"destination:lang\|"gvr:code":\|"int_name":\|"is_in:country_code":\|"wikipedia:\|"code":\|"seasonal":\|"short_name:\|"order:\|"distance":/d' > $OUTPUTFILE
