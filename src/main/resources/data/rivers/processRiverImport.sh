a22311c97394ef54cf4d1f5d94dbfc197ab403c2
Ben Parkinson <benjamesparkinson@gmail.com>
2023-04-17 22:06:47 +0200

    Add more rivers

--- src/main/resources/data/rivers/processRiverImport.sh
+++ src/main/resources/data/rivers/processRiverImport.sh
@@ -3,4 +3,4 @@
 INPUTFILE=$1
 OUTPUTFILE=$2.geo.json
 
-tail -n +7 $INPUTFILE | head -n -2 | sed '/"@id":\|"alt_name:ku":\|"name:\|"id":\|"source:name\|"wikidata":\|"wikipedia":\|"alt_name":\|"alt_name:\|"destination:lang\|"gvr:code":\|"int_name":\|"is_in:country_code":\|"wikipedia:\|"code":\|"seasonal":\|"short_name:\|"order:\|"distance":/d' > $OUTPUTFILE
+tail -n +7 $INPUTFILE | head -n -2 | sed '/"@id":\|"alt_name:ku":\|"name:\|"id":\|"source:name\|"wikidata":\|"wikipedia":\|"alt_name":\|"alt_name:\|"destination:lang\|"gvr:code":\|"int_name":\|"is_in:country_code":\|"wikipedia:\|"code":\|"seasonal":\|"short_name:\|"order:\|"distance":/d' | sed 's/"waterway": "river",/"waterway": "river"/g' > $OUTPUTFILE
